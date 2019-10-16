c4c64ffbcddcd24d700ee5503ebc9b273e391507
Ramil <vafarulez@ya.ru>
2019-10-16 21:32:35 +0400

    pretter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App/App";
