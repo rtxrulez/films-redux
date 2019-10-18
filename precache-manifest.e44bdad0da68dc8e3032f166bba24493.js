267d11124c249799d7a4dcb7a233b9bf5d988d3b
rtxrulez <vafarulez@ya.ru>
2019-10-18 13:59:31 +0300

    Updates

---
+++ precache-manifest.e44bdad0da68dc8e3032f166bba24493.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7379c89c565e0fcf0b0b750379d2ec33",
+    "url": "/films-redux/build/index.html"
+  },
+  {
+    "revision": "a24fd8bd168a9c95e96c",
+    "url": "/films-redux/build/static/css/main.f11ad088.chunk.css"
+  },
+  {
+    "revision": "887bb8a18aeb0403ef69",
+    "url": "/films-redux/build/static/js/2.89905125.chunk.js"
+  },
+  {
+    "revision": "a24fd8bd168a9c95e96c",
+    "url": "/films-redux/build/static/js/main.1bddf207.chunk.js"
+  },
+  {
+    "revision": "0541214baaf13eaba6b1",
+    "url": "/films-redux/build/static/js/runtime-main.87d80784.js"
+  }
+]);
